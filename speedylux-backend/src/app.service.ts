17c3dd2dadb6c6a2c8ef209a8dbb6c726aeca076
Nabil hakim <hakimbsns@gmail.com>
2022-07-21 14:52:14 +0800

    testv4

--- speedylux-backend/src/app.service.ts
+++ speedylux-backend/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! test3';
+    return 'Hello World! ';
   }
 
   // create, get, update and delete user
